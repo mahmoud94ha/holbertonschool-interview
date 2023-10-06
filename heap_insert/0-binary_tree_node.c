efd46edd8493acca5f0066d6e11b25755a6387f7
unknown <mahmoudhannani3@gmail.com>
2023-10-06 11:12:48 +0100

    task 0

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Create new node
+ *
+ * @parent: Pointer to the node parent
+ * @value: value of node
+ *
+ * Return: Pointer of the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
